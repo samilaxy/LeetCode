08a15e34290164833be307e5b8651af1aefc5c3d
Noye Samuel <59480282+samilaxy@users.noreply.github.com>
2023-02-13 09:39:58 +0000

    Update longestCommonPrefix.swift

--- LeetCode/longestCommonPrefix.swift
+++ LeetCode/longestCommonPrefix.swift
@@ -6,3 +6,28 @@
 //
 
 import Foundation
+
+class longestCommonPrefixs {
+    func longestCommonPrefix(_ strs: [String]) -> String {
+        
+        if strs.isEmpty {
+            return ""
+        }
+        if var prefixStr = strs.first {
+            
+            for index in 1..<strs.count {
+                let str = strs[index]
+                while str.contains(prefixStr) == false {
+                    let index = prefixStr.index(prefixStr.startIndex, offsetBy: prefixStr.count - 1)
+                    prefixStr = String(prefixStr[..<index])
+                    if prefixStr.count == 0 {
+                        return ""
+                    }
+                }
+            }
+            return prefixStr
+        }
+        
+        return ""
+    }
+}
