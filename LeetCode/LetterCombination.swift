214f5e307548f2f38ded37ed200732af6330c5ab
Noye Samuel <59480282+samilaxy@users.noreply.github.com>
2023-02-13 09:39:15 +0000

    longestCommonPrefix

--- LeetCode/LetterCombination.swift
+++ LeetCode/LetterCombination.swift
@@ -6,3 +6,27 @@
 //
 
 import Foundation
+
+class LetterCombination {
+    func letterCombinations(_ digits: String) -> [String] {
+        var result = [String]()
+        guard !digits.isEmpty else { return result }
+        letterCombinations(digits: digits, depth: 0, input: "", result: &result)
+        return result
+    }
+    
+    private func letterCombinations(digits: String, depth: Int, input: String, result: inout [String]) {
+        guard depth < digits.count else {
+            result.append(input)
+            return
+        }
+        let numbers = ["2": "abc", "3": "def", "4": "ghi", "5": "jkl", "6": "mno", "7": "pqrs", "8": "tuv", "9": "wxyz", "0": " "]
+        let digit = String(digits[String.Index(encodedOffset: depth)])
+        guard let letters = numbers[digit] else { fatalError("Can't find letters for the digit \(digit)") }
+        
+        for letter in letters {
+            let newInput = input + String(letter)
+            letterCombinations(digits: digits, depth: depth+1, input: newInput, result: &result)
+        }
+    }
+}
---
+++ LeetCode/longestCommonPrefix.swift
@@ -0,0 +1,8 @@
+//
+//  longestCommonPrefix.swift
+//  LeetCode
+//
+//  Created by Noye Samuel on 13/02/2023.
+//
+
+import Foundation
